ca17fe7d4ea1b01a2f18ea9b2abe6d4e89365f8d
Shuja Hassan <shujahassan362@gmail.com>
2026-01-23 18:06:37 +0500

    Fix routing for GitHub viteconfig changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: '/AhsanBari/',
-  plugins: [tailwindcss()],
-})
+  base: "/AhsanBari/",
+  plugins: [react(), tailwindcss()],
+});
